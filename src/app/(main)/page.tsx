13ee40f767a8a367eed01f81ac60d755c4df08e4
Bima1301 <bima.aji1380@gmail.com>
2024-08-17 06:03:43 +0700

    [FEAT] add show posts from following

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,4 +1,6 @@
 import TrendsSidebar from "@/components/layout/TrendsSidebar";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import FollowingFeed from "@/components/views/(main)/home/FollowingFeed";
 import ForYouFeed from "@/components/views/(main)/home/ForYouFeed";
 import PostEditor from "@/components/views/(main)/home/PostEditor";
 
@@ -8,7 +10,18 @@ export default function Home() {
     <main className="w-full min-w-0 flex gap-5">
       <div className="w-full min-w-0 space-y-5">
         <PostEditor />
-        <ForYouFeed />
+        <Tabs defaultValue="for-you">
+          <TabsList className="h-12 w-full gap-1 shadow-sm bg-card">
+            <TabsTrigger value="for-you" className="h-full flex-1 hover:bg-background data-[state=active]:font-bold">For You</TabsTrigger>
+            <TabsTrigger value="following" className="h-full flex-1 hover:bg-background data-[state=active]:font-bold">Following</TabsTrigger>
+          </TabsList>
+          <TabsContent value="for-you">
+            <ForYouFeed />
+          </TabsContent>
+          <TabsContent value="following">
+            <FollowingFeed />
+          </TabsContent>
+        </Tabs>
       </div>
       <TrendsSidebar />
     </main>
---
+++ src/app/api/posts/following/route.ts
@@ -0,0 +1,46 @@
+import { validateRequest } from "@/auth";
+import prisma from "@/lib/prisma";
+import { getPostDataInclude, PostsPage } from "@/lib/types";
+import { NextRequest } from "next/server";
+
+const PAGE_SIZE = 10;
+
+export async function GET(req: NextRequest) {
+    try {
+        const cursor = req.nextUrl.searchParams.get("cursor") || undefined;
+
+        const { user } = await validateRequest()
+
+        if (!user) {
+            return Response.json({ error: "Unauthorized" }, { status: 401 })
+        }
+
+        const posts = await prisma.post.findMany({
+            where: {
+                user: {
+                    followers: {
+                        some: {
+                            followerId: user.id
+                        }
+                    }
+                }
+            },
+            orderBy: { createdAt: "desc" },
+            take: PAGE_SIZE + 1,
+            cursor: cursor ? { id: cursor } : undefined,
+            include: getPostDataInclude(user.id)
+        })
+
+        const nextCursor = posts.length > PAGE_SIZE ? posts[PAGE_SIZE].id : null
+
+        const data: PostsPage = {
+            posts: posts.slice(0, PAGE_SIZE),
+            nextCursor
+        };
+
+        return Response.json(data);
+    } catch (error) {
+        console.error(error)
+        return Response.json({ error: "An error occurred" }, { status: 500 })
+    }
+}
