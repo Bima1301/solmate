4f071032ef49cf2de874c2e0bf8cd9c1c5014584
Bima1301 <bima.aji1380@gmail.com>
2024-08-26 20:18:27 +0700

    [FEAT] add user tooltip for taged user and display name

--- src/components/views/(main)/home/Linkify.tsx
+++ src/components/views/(main)/home/Linkify.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import { LinkIt, LinkItUrl } from 'react-linkify-it'
+import UserLinkWithTooltip from './UserLinkWithTooltip'
 
 interface LinkifyProps {
     children: React.ReactNode
@@ -29,11 +30,10 @@ function LinkifyUsername({ children }: LinkifyProps) {
     return (
         <LinkIt regex={/(@[a-zA-Z0-9_-]+)/}
             component={(match, key) => (
-                <Link key={key} href={`/users/${match.slice(1)}`} className='text-primary hover:underline'>
+                <UserLinkWithTooltip key={key} username={match.slice(1)}>
                     {match}
-                </Link>
-            )
-            }
+                </UserLinkWithTooltip>
+            )}
         >
             {children}
         </LinkIt>
--- src/components/views/(main)/home/PostItem.tsx
+++ src/components/views/(main)/home/PostItem.tsx
@@ -7,6 +7,7 @@ import { formatRelativeDate } from "@/lib/utils"
 import Link from "next/link"
 import PostMoreButton from "./PostMoreButton"
 import Linkify from "./Linkify"
+import UserTooltip from "./UserTooltip"
 
 
 interface PostItemProps {
@@ -20,13 +21,17 @@ export default function PostItem({ post }: PostItemProps) {
         <article className="group/post space-y-3 rounded-[8px] bg-card p-5 shadow-sm">
             <div className="flex justify-between gap-3">
                 <div className="flex flex-wrap gap-3 items-center">
-                    <Link href={`/user/${post.user.username}`} >
-                        <UserAvatar avatarUrl={post.user.avatarUrl} className="w-full h-full max-w-[40px]" />
-                    </Link>
-                    <div>
-                        <Link href={`/users/${post.user.username}`} className="block font-medium hover:underline">
-                            {post.user.displayName}
+                    <UserTooltip user={post.user}>
+                        <Link href={`/user/${post.user.username}`} >
+                            <UserAvatar avatarUrl={post.user.avatarUrl} className="w-full h-full max-w-[40px]" />
                         </Link>
+                    </UserTooltip>
+                    <div>
+                        <UserTooltip user={post.user}>
+                            <Link href={`/users/${post.user.username}`} className="block font-medium hover:underline">
+                                {post.user.displayName}
+                            </Link>
+                        </UserTooltip>
                         <Link href={`/posts/${post.id}`} className="block text-sm text-muted-foreground hover:underline">
                             {formatRelativeDate(post.createdAt)}
                         </Link>
